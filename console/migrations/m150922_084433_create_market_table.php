d2e1589b1dcf8f3f857558d86d959e1ff739bc27
root <root@PayQR-Sites.PayQR-Sites.a5.internal.cloudapp.net>
2015-09-22 08:46:24 +0000

    create migrations

---
+++ console/migrations/m150922_084433_create_market_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150922_084433_create_market_table extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m150922_084433_create_market_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
