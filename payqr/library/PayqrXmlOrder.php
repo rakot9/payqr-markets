c8972337d68c76450a62f27cc564cd2d137a59f2
rakot9 <sobolev@payqr.ru>
2015-11-09 18:26:12 +0300

    payqr v1.2

--- payqr/library/PayqrXmlOrder.php
+++ payqr/library/PayqrXmlOrder.php
@@ -35,7 +35,7 @@ class PayqrXmlOrder {
     
     public function changeOrderPayStatus()
     {
-        $userData = $this->invoice->getUserData();
+        $userData = $this->invoice->userData();
         
         if(isset($userData->orderId) && !empty($userData->orderId))
         {
