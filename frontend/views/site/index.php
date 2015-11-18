342fe584270ab5f72bd4acdad70f71e2e0f4e356
rakot9 <sobolev@payqr.ru>
2015-11-18 11:01:06 +0300

    Hide unauthorized grid

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -13,29 +13,35 @@ $this->title = 'PayQR редактор кнопок';
 
     <div class="body-content">
         <div class="row">
-            <h2>Здесь представлены настройки кнопок пользователя</h2>
-            <?php
-            echo GridView::widget([
-                'dataProvider' => $dataProvider,
-                'columns' => [
-                    'id',
-                    'name',
-                    [
-                        'class' => yii\grid\ActionColumn::className(),
-                        'template' => '{update} {delete}',
-                        'buttons' =>[ 
-                            'update' => function ($url, $model, $key) {
-                                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', '?r=payqr/button/edit&market_id='.$key);
-                            },
-                            'delete' => function ($url, $model, $key) {
-                                return Html::a('<span class="glyphicon glyphicon-trash"></span>', '?r=payqr/button/delete&market_id='.$key);
-                            },
+            <?php 
+                if(\Yii::$app->user->getId()) { ?>
+                <h2>Здесь представлены настройки кнопок пользователя</h2>
+                <?php
+                echo GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'columns' => [
+                        'id',
+                        'name',
+                        [
+                            'class' => yii\grid\ActionColumn::className(),
+                            'template' => '{update} {delete}',
+                            'buttons' =>[ 
+                                'update' => function ($url, $model, $key) {
+                                    return Html::a('<span class="glyphicon glyphicon-pencil"></span>', '?r=payqr/button/edit&market_id='.$key);
+                                },
+                                'delete' => function ($url, $model, $key) {
+                                    return Html::a('<span class="glyphicon glyphicon-trash"></span>', '?r=payqr/button/delete&market_id='.$key);
+                                },
+                            ]
                         ]
-                    ]
-                ],
-            ]);
-            if($dataProvider->getTotalCount() == 0)
-                echo \yii\bootstrap\Html::a('Создать кнопку', ['payqr/button/edit'], ['class'=>'btn btn-primary']);
+                    ],
+                ]);
+                if($dataProvider->getTotalCount() == 0)
+                    echo \yii\bootstrap\Html::a('Создать кнопку', ['payqr/button/edit'], ['class'=>'btn btn-primary']);
+            }
+            else {
+                echo "Вы незарегистрированный пользователь. Чтобы настроить кнопку пройдите процедуру регистрации/авторизации!";
+            }
             ?>
         </div>
 
