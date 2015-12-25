afe17ec003e7aa1e29833f3a54e620b8d5a36e12
rakot9 <sobolev@payqr.ru>
2015-12-25 17:34:45 +0300

    Refactor

--- frontend/web/js/middlerestsrv.js
+++ frontend/web/js/middlerestsrv.js
@@ -1,186 +1,238 @@
-function initButton(callback, place){
-    
-    if( typeof merchant_id == "undefined")
-    {
-        return false;
-    }
-    
-    if(callback && $.isFunction(callback))
-    {
-        console.log(callback + " isn't function!");
-        
-        return false;
-    }
-    
-    if(!place)
-    {
-        return false;
-    }
-    
-    $.ajax({
-        
-        type: 'GET',
-        
-        url: 'http://insales.payqr-sites.qrteam.ru/?r=api/get&HTTP_X_API_KEY=EW5ERdsfwref23&type=button&callback=' + callback + '&merchant_id=' + merchant_id + '&place=' + place,
-        
-        dataType: "jsonp",
-        
-        jsonpCallback: callback,
-        
-        complete: function()
+var payqrButton = {
+
+    merchant_id : "",
+    token : "",
+    userData: {},
+    button: {},
+
+    init: function(merchant_id, token){
+        this.merchant_id = merchant_id;
+        this.token = token;
+        this.createButton();
+    },
+
+    initButtonParams: function(){
+        this.setUserData();
+        this.button['data-scenario']   = 'pay';
+        this.button['data-merchId']    = this.merchant_id;
+        this.button['data-amount']     = this.getTotal();
+        this.button['data-userdata']   = JSON.stringify(this.getUserData());
+        this.button['data-cart']       = JSON.stringify(this.getDataCart());
+        this.button['data-orderIdRequired'] = 'required';
+
+        //устанавливаем в локальное хранилище
+        sessionStorage.setItem('button', JSON.stringify(this.button));
+    },
+
+    createButton: function(){
+
+        /**
+         * Перед созданием кнопки проинициализируем ее параметры
+         */
+        this.initButtonParams();
+
+        if($("#create_order").length > 0)
         {
-            console.log("complete");
-        },
-        
-        success: function(response)
-        {
-            console.log("success");
-            
-            if(typeof response ==  "object")
+            if($("button[class*=payqr-button]").length > 0)
             {
-                data = response;
+                this.setButtonParams();
 
-//                if(typeof data.button == "undefined")
-//                {
-//                    console.log("Промежуточный сервер вернул данные в некорректном формате!");
-//                    return false;
-//                }
-//                else
-//                {
-//                    return data.button;
-//                }
-
-                return "";
+                if(!this.checkPaymentIsSelected())
+                {
+                    $("button[class*=payqr-button]").hide();
+                }
+                else
+                {
+                    $("button[class*=payqr-button]").show();
+                }
+                return;
             }
-            if($.parseJSON(response))
+
+            $("#create_order").after("<button class=\"payqr-button\">Купить быстрее</button>");
+
+            this.setButtonParams();
+
+            /**
+             * Определим, выбрана ли платежная система
+             */
+            if(!this.checkPaymentIsSelected())
             {
-                try{
-                    var data = $.parseJSON(response);
+                $("button[class*=payqr-button]").hide();
+            }
+        }
+    },
 
-                    if(typeof data.cart == "undefined" || data.cart.length == 0)
-                    {
-                        console.log("Промежуточный сервер вернул данные в некорректном формате!");
-                        return false;
-                    }
-
-                    return data.cart;
-                }
-                catch(e){
-                    console.log("Не смогли получить данные с промежуточного сервера в нужном формате!");
-                    return false;
+    setButtonParams: function(){
+        if($('button[class^=payqr-button]').length > 0)
+        {
+            for(var buttonParamKey in this.button)
+            {
+                if(this.button.hasOwnProperty(buttonParamKey))
+                {
+                    $('button[class^=payqr-button]').attr(buttonParamKey, this.button[buttonParamKey]);
                 }
             }
-            return false;
         }
-    });
-}
+    },
 
-function callbackButton(data)
-{
-    var button = $('button[class^=payqr-button]');
-    
-    if(data.data && true == data.display)
-    {
-        button.show();
-        
-        if(typeof data.data.class != "undefined")
-        {
-            button.addClass(data.data.class.join(" "));
+    getUserData: function(){
+        return this.userData;
+    },
+
+    setUserData: function(){
+
+        var sessStoreButton = sessionStorage.getItem('button');
+
+        try {
+            this.button = JSON.parse(sessStoreButton);
+            this.userData = (typeof this.button['data-userdata'] == "object" )? this.button['data-userdata'] : JSON.parse(this.button['data-userdata']);
         }
-        if(typeof data.data.style != "undefined")
+        catch(e)
         {
-            button.attr("style", data.data.style.join(";"));
+            console.error("Не смогли получить данные");
         }
-        if(typeof data.data.attr != "undefined")
+
+        if($("#client_name").length > 0)
         {
-            button.attr(data.data.attr);
+            var name = $("#client_name").val().replace(/( ){1,}/g, " ").split(" ");
+
+            if(typeof name[1] !== "undefined" && name[1].length >0)
+            {
+                this.userData.firstName = name[1];
+            }
+            if(typeof name[0] !== "undefined" && name[0].length >0)
+            {
+                this.userData.lastName = name[0];
+            }
+            if(typeof name[2] !== "undefined" && name[2].length >0)
+            {
+                this.userData.middleName = name[2];
+            }
+        }
+
+        if($("#client_phone").length > 0 && $("#client_phone").val().length > 0)
+        {
+            this.userData.phone = $("#client_phone").val();
+        }
+
+        if($("#client_email").length > 0 && $("#client_email").val().length > 0)
+        {
+            this.userData.email = $("#client_email").val();
+        }
+
+        if($("#shipping_address_zip").length > 0 && $("#shipping_address_zip").val().length > 0)
+        {
+            this.userData.zip = $("#shipping_address_zip").val();
+        }
+
+        if($("#shipping_address_city").length > 0 && $("#shipping_address_city").val().length > 0)
+        {
+            this.userData.city = $("#shipping_address_city").val();
+        }
+
+        if($("#shipping_address_address").length > 0 && $("#shipping_address_address").val().length > 0)
+        {
+            this.userData.address = $("#shipping_address_address").val();
+        }
+
+        if($("#shipping_address_no_delivery").length < 0)
+        {
+            this.userData.city = null;
+            this.userData.address = null;
+        }
+
+        if($("#delivery input:checked").length > 0)
+        {
+            this.userData.deliveryId = $("#delivery input:checked").val();
+        }
+
+        if($("#payment input:checked").length > 0)
+        {
+            this.userData.paymentId = $("#payment input:checked").val();
+        }
+
+        this.userData.token = this.token;
+
+        this.userData.merchId = this.merchant_id;
+    },
+
+    checkPaymentIsSelected: function(){
+        if(($("#order_payment_gateway_id_" + $("#payment_gateways input:checked").attr("data-payment-id")).length > 0 &&
+            $("#order_payment_gateway_id_" + $("#payment_gateways input:checked").attr("data-payment-id")).closest('tr').html().toLowerCase().match(/payqr/i))
+            || ($("#payment input:checked").length > 0 && $("#payment input:checked").closest('tr').html().toLowerCase().match(/payqr/i))
+            )
+        {
+            return true;
+        }
+        return false;
+    },
+
+    getTotal: function (){
+        var total = 0;
+        try {
+            var cart = $.parseJSON($.cookie('cart'));
+            if (cart)
+            {
+                $.each(cart.order_lines, function(index, order_line){
+                    total = cart.total_price;
+                })
+            }
+        }
+        catch(e){
+            console.error("Не смогли обработать данные в корзине!");
+        }
+        return total;
+    },
+
+    getDataCart: function(){
+        var payqr_data_cart = [];
+        try {
+            var cart = $.parseJSON($.cookie('cart'));
+            if (cart)
+            {
+                $.each(cart.order_lines, function(index, order_line){
+                    var item = {};
+                    item['article']  = order_line.variant_id;
+                    item['name']     = order_line.title;
+                    item['quantity'] = order_line.quantity;
+                    item['amount']   = order_line.sale_price * order_line.quantity;
+                    item['imageUrl'] = order_line.image_url;
+                    payqr_data_cart.push(item);
+                })
+            }
+        }
+        catch(e){
+            console.error("Не смогли обработать данные в корзине!");
+        }
+        return payqr_data_cart;
+    },
+
+    clearUserData: function(){
+        return {
+            "data-userdata":{
+                                "firstName":"",
+                                "lastName":"",
+                                "middleName":"",
+                                "phone":"",
+                                "email":"",
+                                "zip":"",
+                                "city":"",
+                                "address":"",
+                                "token":"",
+                                "merchId":"",
+                                "deliveryId":"",
+                                "paymentId":""
+            }
         }
     }
-    else
-    {
-        button.hide();
-    }
-}
-    
-function setCart(buttonPlace)
-{
-    var basketItems = initButton("callbackButton", buttonPlace);
-    
-    if(typeof basketItems == "Array")
-    {
-        console.log("Basket items ia array");
-    }
-}
-
-function setProduct()
-{
-    initButton("callbackButton", "product");
-}
-
-function RefreshDataCart()
-{
-    var button = $('button[class^=payqr-button]');
-    
-    var cart = $.parseJSON($.cookie('cart'));
-    
-    var price  = 0;
-    
-    var count = 0;    
-    
-    var payqr_data_cart = [];
-    
-    if (cart) {
-
-        price = cart.total_price;
-        
-        $.each(cart.order_lines, function(index, order_line) 
-        {
-            var item = {};
-            
-            count += order_line.quantity;
-            
-            item['article'] = order_line.variant_id;
-            item['name'] = order_line.title;
-            item['quantity'] = order_line.quantity;
-            item['amount'] = order_line.sale_price * order_line.quantity;
-            item['imageUrl'] = order_line.image_url;
-            
-            payqr_data_cart.push(item);
-        });
-    }
-    
-    button.attr('data-amount', price);
-    
-    button.attr('data-cart', JSON.stringify(payqr_data_cart));
 }
 
 $(function(){
-    if($("button[class*=payqr-button]").length > 0)
-    {
-        if($("#cartform").length)
-        {
-            setCart("cart");
-        }
-        else if($("div[class=product_preview-preview]").length)
-        {
-            setCart("category")
-        }
-        else if($("div[class=product_preview-preview]").length==0 && $("div[class=product_preview-preview]").length ==0)
-        {
-            setCart("product");
-        }
-    }
-    
-    $(document).ajaxComplete(function(event, xhr, settings){
-        
-        if(settings.url.indexOf('/cart_items/') != -1)
-        {
-            RefreshDataCart();
-        }
-    });
-    
-    if(typeof payQR  !== "undefined")
-    {
+    /**
+     * Производим обработку поступившего ответа от сервера
+     */
+    if(typeof payQR  !== "undefined"){
+
         payQR.onPaid(function(data) {
 
             var message = "Ваш заказ #" + data.orderId + " успешно оплачен на сумму: " + data.amount + "! ";
@@ -218,4 +270,38 @@ $(function(){
             }
         });
     }
+
+    /**
+     * Перерисовываем кнопку при изменении параметров "оформления заказа"
+     */
+    $(document).ajaxComplete(function(event, xhr, settings){
+        if(settings.url.match(/payment\/for_order/i))
+        {
+            window.sessionStorage.setItem('button', JSON.stringify(payqrButton.clearUserData()));
+            payqrButton.init(merchant_id, insales);
+        }
+    });
+
+    payqrButton.init(merchant_id, insales);
+    /**
+     * Событие на обновление таблицы
+     */
+    $('#order_form, #contacts, #delivery, #payment').on('change', function(){
+        if(this.id == "contacts")
+        {
+            window.sessionStorage.setItem('button', JSON.stringify(payqrButton.clearUserData()));
+        }
+        payqrButton.init(merchant_id, insales);
+    });
+
+    /**
+     * При нажатии на скнопку "Продолжить"
+     */
+    $('#contacts, #delivery, #payment').submit(function(){
+        if(this.id == "contacts")
+        {
+            window.sessionStorage.setItem('button', JSON.stringify(payqrButton.clearUserData()));
+        }
+        payqrButton.init(merchant_id, insales);
+    });
 });
